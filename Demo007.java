c058fbfef47d8c4594445dd13e665679fd05a5db
gillela-1234 <2311CS010224@mallareddyuniversity.ac.in>
2025-02-01 11:05:51 +0530

    user information of name and phone number

---
+++ Demo007.java
@@ -0,0 +1,11 @@
+public class Demo007 {
+    public static void main (String[] args){
+        String username= "Sravanthi06";
+        String pwd="sravs4ever06";
+        while((username == "Sravanthi06") && (pwd == "sravs4ever06")) {
+            System.out.println("Hello Sravanthi you are welcome");
+            break;
+        }
+    }
+    
+}
