6c92fc091b800e00f2dcbc2c177c69976a05cb2b
gillela-1234 <2311CS010224@mallareddyuniversity.ac.in>
2025-02-06 11:13:17 +0530

    swap to first and last element of an array

--- Demo064.java
+++ Demo064.java
@@ -1,12 +1,14 @@
-import java.util.Arrays;
-
 public class Demo064 {
     public static void main(String[] args) {
-        int[] arr = {2, 3, 5, 7};
-        System.out.println("Before Swapping: " + Arrays.toString(arr));
-        arr[0] = arr[0] + arr[3];
-        arr[3] = arr[0] - arr[3];
-        arr[0] = arr[0] - arr[3];
-        System.out.println("After Swapping: " + Arrays.toString(arr));
+        int[] array = {10, 20, 30, 40, 50};
+        System.out.println("Original Array: " + java.util.Arrays.toString(array));
+        swapFirstLast(array);
+        System.out.println("Array after swapping: " + java.util.Arrays.toString(array));
+    }
+
+    public static void swapFirstLast(int[] array) {
+        int temp = array[0];
+        array[0] = array[array.length - 1];
+        array[array.length - 1] = temp;
     }
 }
